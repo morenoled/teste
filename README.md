82e7ccf7b3fb1a34286bfc93e9e7bb4575bb3588
Roberto Haddock Lobo <rhlobo+github@gmail.com>
2021-02-19 13:23:12 -0300

    Aplicação Web básico
    Responde "Hello World" na URL padrão e se tiver algo depois da / usa isso como nome do usuário.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# teste
+Teste
+=====
+
 um dois três testando, som
+
+
+## Instalação
+
+Executar `pip install -r requirements.txt` instala as dependencias
+
+## Execução
+
+Na primeira execução deste terminal, executar:
+$ `export FLASK_ENV=development`
+
+Para rodar o programa:
+$ `FLASK_APP=main.py flask run`
+
+## Estudar
+
+- [ ] [https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application](flask): Entender o que eu fiz no arquivo main.py
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+'''
+The route() decorator to tell Flask what URL should trigger our function.
+The function is given a name which is also used to generate URLs for that particular function, 
+and returns the message we want to display in the user’s browser.
+'''
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/<username>')
+def hello_user(username):
+    return f'Hello, {username}!'
